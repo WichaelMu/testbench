408bbbd47e89558ff9846355806c585b4105669b
WichaelMu <w.michael.u@gmail.com>
2024-05-02 14:56:13 +1000

    Add Missing Counts

--- excel/missing/missing.py
+++ excel/missing/missing.py
@@ -18,13 +18,19 @@ def exec():
         SecondInput = input();
     
     print (F"L: {len (InLArray)}, R: {len (InRArray)}");
+    NotL = 0;
     for L in InLArray:
         if L not in InRArray:
             print (F"{L} is not in SecondInput");
+            NotL = NotL + 1;
 
+    NotR = 0;
     for R in InRArray:
         if R not in InLArray:
             print (F"{R} is not in FirstInput");
+            NotR = NotR + 1;
+
+    print (F"Deltas:\n\tFirstInput not in SecondInput: {NotL}\n\tSecondInput not in FirstInput: {NotR}");
 
 if (__name__ == "__main__"):
     exec();
