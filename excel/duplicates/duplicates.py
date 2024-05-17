7904b9b1c01c54679398b97e7246a5cf77ecb479
WichaelMu <w.michael.u@gmail.com>
2024-05-17 16:09:36 +1000

    Counters for Duplicates

--- excel/duplicates/duplicates.py
+++ excel/duplicates/duplicates.py
@@ -11,9 +11,17 @@ def exec():
         In = input();
 
     print ("----- Printing Duplicates -----");
+    Dupes = 0;
+    Rows = 0;
     for K, V in Duplicates.items():
         if (V > 1):
             print (F"{K} - {V}");
+            Dupes = Dupes + 1;
+            Rows = Rows + V - 1;
+
+    print ("----- Printing Results -----");
+    print (F"\tThere are {Dupes} duplicates.");
+    print (F"\tAccounting for {Rows} rows.");
 
 if (__name__ == "__main__"):
     exec();
