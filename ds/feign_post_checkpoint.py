446c81a7306b0715906eb55fa41efe2ff87c2170
WichaelMu <w.michael.u@gmail.com>
2025-05-08 13:30:42 +1000

    Feign Post Checkpoint State Sanity Check. Still returns 403...

---
+++ ds/feign_post_checkpoint.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import requests
+
+import dscore as dsc
+
+def get_secret_stuff ():
+    secret_stuff = dsc.load_json ('feign_post_checkpoint_secrets.json')['NONPROD']
+
+    client_id = secret_stuff['client_id']
+    client_secret = secret_stuff['client_secret']
+    endpoint = secret_stuff['endpoint']
+    oidc_token_endpoint = secret_stuff['oidc_token_endpoint']
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+
+    response = requests.post (oidc_token_endpoint, data=payload)
+    response.raise_for_status ()
+    token_data = response.json ()
+
+    return token_data['access_token']
+
+def main ():
+    access_token = get_secret_stuff ()
+
+    target_url = 'https://uts-test.t1cloud.com/Api/WS/v1/SystemVariable/Save'
+
+    payload = {
+        'ParameterName': 'EVT_LAST_MOD_SSP',
+        'Value': '2025-02-04T06:02:49+11:00'
+    }
+
+    headers = { 'Authorization': F'Bearer: {access_token}' }
+
+    response = requests.post (target_url, headers=headers)
+    # response.raise_for_status ()
+
+    print (response.json ())
+
+if (__name__ == '__main__'):
+    main ();
+    sys.exit (0)
