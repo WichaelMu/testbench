78a7b332609b5cc040142508998ad2c98d50251c
WichaelMu <w.michael.u@gmail.com>
2026-04-14 16:07:16 +1000

    More testing of the Rule Engine

--- ds/ren.py
+++ ds/ren.py
@@ -3,16 +3,33 @@ import os
 import rule_engine as ren
 import dscore as dsc
 
-PAYLOAD = {
-  "documents": [
-    { "type": "invoice", "fields": { "email": "john@doe.com", "taxId": "" } },
-    { "type": "id",      "fields": { "email": "",             "taxId": "123-45-6789" } }
-  ]
-}
+from functional import pseq
+
+def read_from_tags (tags, what):
+    what += '='
+    readed = pseq (tags) \
+        .filter (lambda r: r[0:len (what)] == what) \
+        .map (lambda r: r[len (what):]) \
+        .to_list ()
+
+    if (len (readed) == 0):
+        return F'Failed to read {what}'
+
+    return readed[0]
+
+def construct_tags (tags):
+    return pseq (tags) \
+        .map (lambda t: t.split ('=', 1)) \
+        .to_dict ()
 
 def main ():
-    result = ren.run_rule_engine (PAYLOAD)
-    print (result)
+    result = ren.run_rule_engine (dsc.load_json ('rules/input.json'))
+
+    tags = result['tags']
+    print (construct_tags (tags))
+
+    readed = read_from_tags (tags, 'CourseCode')
+    # print (readed)
 
 if (__name__ == '__main__'):
     main ()
